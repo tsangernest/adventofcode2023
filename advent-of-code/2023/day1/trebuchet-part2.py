b4fcdca40af40d5c74acf60a1626b01800fb0939
tsangernest <tsangernest@hotmail.com>
2023-12-14 21:07:23 -0600

    * something isn't quite right

--- advent-of-code/2023/day1/trebuchet-part2.py
+++ advent-of-code/2023/day1/trebuchet-part2.py
@@ -1,62 +1,42 @@
-from collections import OrderedDict
-import re
-
-from pprint import PrettyPrinter
-pp = PrettyPrinter(2, 1)
-
-
-
 """
 ===================================================
 In this example, the calibration values are:
-29, 83, 13, 24, 42, 14, and 76. 
+29, 83, 13, 24, 42, 14, and 76.
 Adding these together produces 281.
-
 
 answer = ?
 ===================================================
 """
+from pprint import PrettyPrinter
+from re import Pattern
+import re
+
+pp = PrettyPrinter(indent=2, width=1)
 
 
-
-NUM_ALPHA: dict = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
-
-NUM_ALPHA: dict = OrderedDict(NUM_ALPHA)
-
-
-
-def get_file_data() -> dict:
-    with open(file="part2-sample-input",
-              mode="r",
-              encoding="utf-8") as file:
-        return {key: value.strip() for key, value in enumerate(file)}
+nums: str = "one|two|three|four|five|six|seven|eight|nine"
+nums_re: Pattern = re.compile(r"(?=(\d|%s))" % nums)
+nums: list = nums.split("|")
 
 
 
 
-def main() -> None:
-    input_file: dict = get_file_data()
 
-    for line in input_file.values():
-        for char in line:
-            pass
+with open(file=f"part2-input",
+          mode="r",
+          encoding="utf-8") as f:
+    total: int = 0
 
+    for line in f:
+        digits: list = []
 
-    print(f"\n***In The End***\n")
+        for num in nums_re.findall(line):
+            if num in nums:
+                num: str = str(nums.index(num) + 1)
 
+            digits.append(num)
 
+        total += int(digits[0] + digits[-1])
 
-if __name__ == "__main__":
-    main()
-
+    pp.pprint(object=total)
 
